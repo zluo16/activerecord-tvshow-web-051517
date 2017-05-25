c5370b662ae9adbb5bee2e052166bcaf8a65e5c3
zluo16 <zluo@alumni.berklee.edu>
2017-05-25 09:01:32 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,9 +2,32 @@ require "pry"
 
 class Show < ActiveRecord::Base
 
-  def highest_rating
-    binding.pry
-    Show.all
+  def self.highest_rating
+    self.all.map{|s| s.rating}.sort.last
+  end
+
+  def self.most_popular_show
+    self.find_by(rating: highest_rating)
+  end
+
+  def self.lowest_rating
+    self.all.map{|s| s.rating}.sort.first
+  end
+
+  def self.least_popular_show
+    self.find_by(rating: lowest_rating)
+  end
+
+  def self.ratings_sum
+    self.all.map{|s| s.rating}.inject(0){|sum, x| sum + x}
+  end
+
+  def self.popular_shows
+    self.all.select{|s| s.rating > 5}
+  end
+
+  def self.shows_by_alphabetical_order
+    self.all.sort_by{|s| s.name.downcase}
   end
 
 end
